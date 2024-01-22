d62c8a91badf588108c6b9b0f345a13afe695f7c
Vaibhav-zluri <vaibhav.k@zluri.com>
2024-01-22 12:53:47 +0530

    added more valid currency

--- scheme.js
+++ scheme.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const validCurrencies = ["USD", "EUR", "GBP", "INR"]; // Add more currencies as needed
+const validCurrencies = ['INR', 'AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN', 'BAM', 'BBD', 'BDT', 'BGN', 'BHD', 'BIF', 'BMD', 'BND', 'BOB', 'BRL', 'BSD', 'BTN', 'BWP', 'BYN', 'BZD', 'CAD', 'CDF', 'CHF', 'CLP', 'CNY', 'COP', 'CRC', 'CUP', 'CVE', 'CZK', 'DJF', 'DKK', 'DOP', 'DZD', 'EGP', 'ERN', 'ETB', 'EUR', 'FJD', 'FKP', 'FOK', 'GBP', 'GEL', 'GGP', 'GHS', 'GIP', 'GMD', 'GNF', 'GTQ', 'GYD', 'HKD', 'HNL', 'HRK', 'HTG', 'HUF', 'IDR', 'ILS', 'IMP', 'IQD', 'IRR', 'ISK', 'JEP', 'JMD', 'JOD', 'JPY', 'KES', 'KGS', 'KHR', 'KID', 'KMF', 'KRW', 'KWD', 'KYD', 'KZT', 'LAK', 'LBP', 'LKR', 'LRD', 'LSL', 'LYD', 'MAD', 'MDL', 'MGA', 'MKD', 'MMK', 'MNT', 'MOP', 'MRU', 'MUR', 'MVR', 'MWK', 'MXN', 'MYR', 'MZN', 'NAD', 'NGN', 'NIO', 'NOK', 'NPR', 'NZD', 'OMR', 'PAB', 'PEN', 'PGK', 'PHP', 'PKR', 'PLN', 'PYG', 'QAR', 'RON', 'RSD', 'RUB', 'RWF', 'SAR', 'SBD', 'SCR', 'SDG', 'SEK', 'SGD', 'SHP', 'SLE', 'SLL', 'SOS', 'SRD', 'SSP', 'STN', 'SYP', 'SZL', 'THB', 'TJS', 'TMT', 'TND', 'TOP', 'TRY', 'TTD', 'TVD', 'TWD', 'TZS', 'UAH', 'UGX', 'USD', 'UYU', 'UZS', 'VES', 'VND', 'VUV', 'WST', 'XAF', 'XCD', 'XDR', 'XOF', 'XPF', 'YER', 'ZAR', 'ZMW', 'ZWL']; 
 
 const expensescheme = new mongoose.Schema({
     Date: {
@@ -32,14 +32,14 @@ const expensescheme = new mongoose.Schema({
         default: "USD",
         validate: {
             validator: function (currency) {
-                return validCurrencies.includes(currency.toUpperCase()); // Case-insensitive check
+                return validCurrencies.includes(currency.toUpperCase()); 
             },
             message: "Invalid currency"
         }
     },
 });
 
-// Add a pre-save hook to format the date before saving to the database
+
 
 
 const SchemeExpenses = mongoose.model("Expensecollection", expensescheme);
